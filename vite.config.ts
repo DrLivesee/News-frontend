b77dbe81e0554897e9ad810f8d74dd3374a210dc
Dr Livesee <evgenijmanaenkov@gmail.com>
2024-04-22 12:37:18 +0300

    refactor: add scss files

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,23 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
-    port: 8080
+    port: 8080,
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/assets/scss/variables.scss"; @import "@/assets/scss/mixins.scss";`,
+      },
+    },
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
