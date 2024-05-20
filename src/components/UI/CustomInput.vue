8307304e86741a51c2b0df5daba6da62e4f68f31
Evgenii Manaenkov <evgenijmanaenkov@gmail.com>
2024-05-20 15:56:22 +0300

    feat: add custom input component

---
+++ src/components/UI/CustomInput.vue
@@ -0,0 +1,32 @@
+<template>
+  <input
+    :type="type"
+    class="input"
+    :placeholder="placeholder"
+    v-model="model"
+    :maxlength="maxlength"
+    
+  />
+</template>
+
+<script setup lang="ts">
+
+import { CustomInputProps } from "@/interfaces";
+
+withDefaults(defineProps<CustomInputProps>(), {
+  placeholder: "Введите данные",
+  type: "text",
+  maxlength: 10000,
+  
+});
+
+const model = defineModel()
+
+
+</script>
+
+<style lang="scss" scoped>
+.input {
+  @include baseInput;
+}
+</style>
