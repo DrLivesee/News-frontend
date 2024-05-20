fc0825180512aa82172e1441ca825b0943a56f7c
Evgenii Manaenkov <evgenijmanaenkov@gmail.com>
2024-05-20 15:28:05 +0300

    feat: add patch comments

--- src/service/CommentsService.ts
+++ src/service/CommentsService.ts
@@ -1,23 +1,46 @@
 import $comments from "@/http/comments";
 import { AxiosResponse } from "axios";
-import { IComment, ApiCommentsResponse,IPostComment, IPatchComment } from "@/interfaces";
+import {
+  IComment,
+  ApiCommentsResponse,
+  IPostComment,
+  IPatchComment,
+} from "@/interfaces";
 
-export async function getComments(): Promise<AxiosResponse<ApiCommentsResponse>>  {
+export async function getComments(): Promise<
+  AxiosResponse<ApiCommentsResponse>
+> {
   return $comments.get("/");
 }
 
-export async function getCommentById(id: string): Promise<AxiosResponse<IComment>> {
+export async function getCommentById(
+  id: string
+): Promise<AxiosResponse<IComment>> {
   return $comments.get(`/${id}`);
 }
 
-export async function deleteCommentById(id: string): Promise<AxiosResponse<void>> {
+export async function deleteCommentById(
+  id: string
+): Promise<AxiosResponse<void>> {
   return $comments.delete(`/${id}`);
 }
 
-export async function addComment(comment: IPostComment): Promise<AxiosResponse<IComment>> {
+export async function addComment(
+  comment: IPostComment
+): Promise<AxiosResponse<IComment>> {
   return $comments.post("/", comment);
 }
 
-export async function patchComment(commentId: string, comment: IPatchComment): Promise<AxiosResponse<IComment>> {
+export async function patchComment(
+  commentId: string,
+  comment: IPatchComment
+): Promise<AxiosResponse<IComment>> {
   return $comments.patch(`/${commentId}`, comment);
+}
+
+export async function patchComments(
+  ids: string[],
+  updatedFields: Partial<IComment>
+): Promise<AxiosResponse<IComment>> {
+  return $comments.patch("/bulk-update", { ids, updatedFields });
 }
